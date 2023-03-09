fe7403dec6ef43b100dd30fb1dbf256c22f1df20
ItsDan1el <111002559+ItsDan1el@users.noreply.github.com>
2023-03-09 20:57:49 +0800

    Update LoginDialog.java

--- src/com/itsdan1el/zhixuetong/view/LoginDialog.java
+++ src/com/itsdan1el/zhixuetong/view/LoginDialog.java
@@ -15,29 +15,76 @@ public class LoginDialog extends JDialog {
     private JButton cancelBtn = null;
     private final int WIDTH = 300, HEIGHT = 150;
 
-    //Main Frame
-    public LoginDialog(Frame owner, boolean modal){
-        super(owner, "账号登陆", modal);
+    //Main Dialog Pannel
+    public LoginDialog(Frame owner){
+        super(owner, "账号登陆");
         setSize(WIDTH,HEIGHT);
         setLocation(owner.getX() + (owner.getWidth() - WIDTH)/2, owner.getY() + (owner.getHeight() - HEIGHT)/2);
-        loginBtn.addActionListener(new ActionListener(){
+        init();
+        addListener();
+        setVisible(true);
+    }
 
 
 
+    private void init() {
+        JLabel usernameLabel = new JLabel("用户名", JLabel.CENTER);
+        JLabel passwordLabel = new JLabel("密 码", JLabel.CENTER);
+        usernameField = new JTextField();
+        passwordField = new JPasswordField();
+        loginBtn = new JButton("登陆");
+        cancelBtn = new JButton("取消");
+
+        Container c = getContentPane();
+        c.setLayout(new GridLayout(3, 2));
+        c.add(usernameLabel);
+        c.add(usernameField);
+        c.add(passwordLabel);
+        c.add(passwordField);
+        c.add(loginBtn);
+        c.add(cancelBtn);
+    }
+
+    private void addListener() {
+        cancelBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String username = usernameField.getText().trim();
-                String password = new String(passwordField.getPassword());
-                boolean result = AccountService.userLogin(username, password);
-                if(result){
-                    LoginDialog.this.dispose();
-                }else{
-                    JOptionPane.showMessageDialog(LoginDialog.this,"用户民或密码错误！");
-                }
+                LoginDialog.this.dispose();
             }
         });
 
+        loginBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
 
+                String username = usernameField.getText().trim();
+
+                String password = new String(passwordField.getPassword());
+
+                boolean result = AccountService.userLogin(username, password);
+                if (result) {
+                    LoginDialog.this.dispose();
+                } else {
+
+                    JOptionPane.showMessageDialog(LoginDialog.this, "用户名或密码错误！", "提示！",
+                            JOptionPane.PLAIN_MESSAGE);
+
+                }
+            }
+        });
+        passwordField.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                loginBtn.doClick();
+            }
+        });
+
+        usernameField.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                passwordField.grabFocus();
+            }
+        });
     }
 }
 
